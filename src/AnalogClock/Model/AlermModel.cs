de861a1263ccf7e440a545c09e4e497c8c4ecfbc
masjinno <masjinno@gmail.com>
2017-04-27 04:58:35 +0900

    #3 アラームモデルクラスを追加

---
+++ src/AnalogClock/Model/AlermModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalogClock.Model
+{
+    class AlermModel
+    {
+        /// <summary>
+        /// アラームが有効か否か
+        /// true:有効  false:無効
+        /// </summary>
+        public bool IsAlermOn;
+
+        /// <summary>
+        /// アラームに設定されている時刻
+        /// </summary>
+        public int Hour;
+        public int Minute;
+        public int Second;
+
+        /// <summary>
+        /// コンストラクタ。
+        /// ・メンバを初期化する。
+        /// </summary>
+        public AlermModel()
+        {
+            IsAlermOn = false;
+            Hour = 0;
+            Minute = 0;
+            Second = 0;
+        }
+    }
+}
