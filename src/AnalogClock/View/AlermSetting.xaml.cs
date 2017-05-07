30e4f1a9a4cd98d239c4bdae0e8e47e08dafad0b
masjinno <masjinno@gmail.com>
2017-05-08 04:30:03 +0900

    #3 アラーム設定ユーザーコントロール作成

--- src/AnalogClock/View/AlermSetting.xaml.cs
+++ src/AnalogClock/View/AlermSetting.xaml.cs
@@ -20,9 +20,67 @@ namespace AnalogClock.View
     /// </summary>
     public partial class AlermSetting : UserControl
     {
+        #region UserControlイベント
+
+        /// <summary>
+        /// 【UserControl】【Event】
+        /// OKボタンクリックイベント
+        /// </summary>
+        public static RoutedEvent OKButtonClickEvent = EventManager.RegisterRoutedEvent(
+            "OKButtonClick",
+            RoutingStrategy.Tunnel,
+            typeof(RoutedEventHandler),
+            typeof(AlermSetting));
+        public event RoutedEventHandler OKButtonClick
+        {
+            add { this.AddHandler(OKButtonClickEvent, value); }
+            remove { this.RemoveHandler(OKButtonClickEvent, value); }
+        }
+
+        /// <summary>
+        /// 【UserControl】【Event】
+        /// Cancelボタンクリックイベント
+        /// </summary>
+        public static RoutedEvent CancelButtonClickEvent = EventManager.RegisterRoutedEvent(
+            "CancelButtonClick",
+            RoutingStrategy.Tunnel,
+            typeof(RoutedEventHandler),
+            typeof(AlermSetting));
+        public event RoutedEventHandler CancelButtonClick
+        {
+            add { this.AddHandler(CancelButtonClickEvent, value); }
+            remove { this.RemoveHandler(CancelButtonClickEvent, value); }
+        }
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ。
+        /// WPF規定の処理の他に、イベント登録処理を行う。
+        /// </summary>
         public AlermSetting()
         {
+            /// デフォルトの処理
             InitializeComponent();
+            
+            /// イベント登録処理
+            RegisterEvents();
+        }
+
+        private void RegisterEvents()
+        {
+            /// OKボタンクリックイベント登録処理
+            OKButton.Click += (object sender, RoutedEventArgs e) =>
+            {
+                RoutedEventArgs newEventArgs = new RoutedEventArgs(AlermSetting.OKButtonClickEvent);
+                RaiseEvent(newEventArgs);
+            };
+
+            /// Cancelボタンクリックイベント登録処理
+            CancelButton.Click += (object sender, RoutedEventArgs e) =>
+            {
+                RoutedEventArgs newEventArgs = new RoutedEventArgs(AlermSetting.CancelButtonClickEvent);
+                RaiseEvent(newEventArgs);
+            };
         }
     }
 }
